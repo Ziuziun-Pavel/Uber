e3698d9731d0d863dac1d7e6afe75612d88437bf
Pavel <zuzunpavel@gmail.com>
2021-11-09 12:48:16 +0300

    add adoptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
-const btn = document.querySelector('.promo_btn')
+const btn = document.querySelector('.promo_btn'),
+      burger_btn = document.querySelector('.burger_menu'),
+      menuBurger = document.querySelector('.burger_menu'),
+      menu = document.querySelector('.menu');
 
 btn.addEventListener('click', () => {
     alert("Click!")
 });
+
+burger_btn.addEventListener('click', () => {
+    menuBurger.classList.toggle('burger_menu_active');
+    menu.classList.toggle('menu_active');
+})
